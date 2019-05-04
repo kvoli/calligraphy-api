e51de229c3ae61b9dc1cf1c7bb6df8e68641c6e7
kvoli <amcclernon@student.unimelb.edu.au>
2019-05-04 11:39:00 -0400

    add apt buildpackage

--- post.py
+++ post.py
@@ -1,20 +1,10 @@
-import requests, base64, json
+import requests
 
 url = "https://frozen-badlands-62690.herokuapp.com/packages/upload"
 file = "static/cc/bu.png"
 
 
-# with open("./static/cc/bu.png", "rb") as f:
-#     data = f.read()
-#     encoded_string = base64.b64encode(data)
-
 with open(file, 'rb') as f:
     requests.post(url, data=f)
 
-
-# r = requests.post(url=url, data=json.dumps({"name": "test", "img": encoded_string, "package": "chinese.json"}))
-
-# r = requests.post(url=url, data=encoded_string)
-
-# w = requests.get(url=url)
 
